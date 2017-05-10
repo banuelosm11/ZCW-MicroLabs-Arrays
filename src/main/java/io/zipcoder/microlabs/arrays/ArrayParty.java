5087ce59086c219f5d01cf3f68ec1d35e0438613
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-05-10 13:44:18 -0400

    first 3

--- src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
+++ src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
@@ -4,16 +4,49 @@ package io.zipcoder.microlabs.arrays;
 public class ArrayParty {
 
     public String printArray(String[] inputArray){
-        return "";
+
+        String output = "*** Output ***\n";
+
+        for(int i = 0; i < inputArray.length; i++){
+            if(i == inputArray.length - 1) {
+                output += inputArray[i];
+            }else{
+                output += inputArray[i] + "\n";
+            }
+        }
+
+        return output;
     }
 
     public String lastElement(String[] inputArray){
-        return "";
+
+        String output = "*** Output ***\n";
+
+        output += inputArray[inputArray.length - 1];
+
+        return output;
     }
 
     //TODO Define the method lastButOne
 
+    public String lastButOne(String[] inputArray){
+
+        String output = "*** Output ***\n";
+
+        output += inputArray[inputArray.length - 2];
+
+        return output;
+    }
+
     //TODO Define the method reverse
+
+    public String reverse(String[] inputArray){
+
+        String output = "*** Output ***\n";
+
+        
+        return "";
+    }
 
     //TODO Define the method isPalindrome
 
--- src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
+++ src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
@@ -28,15 +28,52 @@ public class ArrayPartyTest {
     @Test
     public void lastElementTest(){
         //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        ArrayParty arrayParty = new ArrayParty();
+        String expected = "*** Output ***\n" +
+                "Mushrooms";
 
         //: When
+        String actual = arrayParty.lastElement(breakfast);
 
         //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
     //TODO Define the method lastButOneTest
 
+    @Test
+    public void lastButOneTest(){
+        //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        ArrayParty arrayParty = new ArrayParty();
+        String expected = "*** Output ***\n" +
+                "Tomatoes";
+
+        //: When
+        String actual = arrayParty.lastButOne(breakfast);
+
+        //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
+    }
+
     //TODO Define the method reverseTest
+
+    @Test
+    public void reverseTest(){
+        //Given:
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        ArrayParty arrayParty = new ArrayParty();
+        String expected = "*** Output ***\n" + "Mushrooms\n" + "Tomatoes\n"+
+                "Bacon\n"+"Beans\n"+"Eggs\n"+ "Sausage\n" + "Tails\n";
+
+        //When:
+        String actual = arrayParty.reverse(breakfast);
+
+        //Then:
+        Assert.assertEquals("The two strings are equal", expected, actual);
+
+    }
 
     //TODO Define the method isPalindromeTest
 
